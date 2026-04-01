dae0777bb3b785fd770d5f19f66f2ca0fa499a6b
witshi <24a4043249@bav.edu.vn>
2026-04-01 16:10:54 +0700

    Svelte + Laravel

---
+++ resources/js/actions/App/Http/Controllers/Settings/index.ts
@@ -0,0 +1,9 @@
+import ProfileController from './ProfileController'
+import SecurityController from './SecurityController'
+
+const Settings = {
+    ProfileController: Object.assign(ProfileController, ProfileController),
+    SecurityController: Object.assign(SecurityController, SecurityController),
+}
+
+export default Settings
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    use RefreshDatabase;
+
+    public function test_that_true_is_true()
     {
         $this->assertTrue(true);
     }
